17da63b2b479e6b888377f425eece87a284a9a47
Leonid Amirov <leonid.amirov@gmail.com>
2024-02-29 21:51:41 +0300

    feat: selecting minimal hamster data with sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open("../hamster_test.db").unwrap();
+    let query = "
+        select start_time, name
+        from facts
+        left join activities
+        on activities.id=facts.activity_id
+        order by facts.id desc
+        limit 5;
+    ";
+    connection
+        .iterate(query, |pairs| {
+            for &(start_date, name) in pairs.iter() {
+                println!("{} = {}", start_date, name.unwrap());
+            }
+            true
+        })
+        .unwrap();
 }
