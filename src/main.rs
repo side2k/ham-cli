5a0df7107771d4b5bdc09ae33cc1b6ad011c9990
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-05 09:01:08 +0300

    feat: simple table output using comfy-table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use comfy_table::Table;
 fn main() {
     let connection = sqlite::open("../hamster_test.db").unwrap();
     let query = "
@@ -9,12 +10,25 @@ fn main() {
         LIMIT 5;
     ";
 
-    fn process_row(pairs: &[(&str, Option<&str>)]) -> bool {
+    let mut table = Table::new();
+
+    table.set_header(vec!["start_time", "name"]);
+
+    let process_row = |pairs: &[(&str, Option<&str>)]| -> bool {
+        let mut fact_dt = String::new();
+        let mut fact_name = String::new();
         for &(name, value) in pairs.iter() {
-            println!("{} = {}", name, value.unwrap());
+            match name {
+                "name" => fact_name = String::from(value.unwrap()),
+                "start_time" => fact_dt = String::from(value.unwrap()),
+                _ => {}
+            }
         }
+        table.add_row(vec![fact_dt, fact_name]);
+
         true
-    }
+    };
 
     connection.iterate(query, process_row).unwrap();
+    println!("{table}");
 }
