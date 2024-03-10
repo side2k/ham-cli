f20f37892c746dd55ae7076bcd75a7dd508e69cf
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-10 17:58:08 +0300

    feat: parse start_date and then serialize it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{Local, NaiveDateTime};
 use comfy_table::Table;
 fn main() {
     let connection = sqlite::open("../hamster_test.db").unwrap();
@@ -11,16 +12,23 @@ fn main() {
     ";
 
     let mut table = Table::new();
+    let local_tz = Local::now().timezone();
 
     table.set_header(vec!["start_time", "name"]);
 
     let process_row = |pairs: &[(&str, Option<&str>)]| -> bool {
-        let mut fact_dt = String::new();
+        let mut fact_dt: String = String::new();
         let mut fact_name = String::new();
         for &(name, value) in pairs.iter() {
             match name {
                 "name" => fact_name = String::from(value.unwrap()),
-                "start_time" => fact_dt = String::from(value.unwrap()),
+                "start_time" => {
+                    fact_dt = NaiveDateTime::parse_from_str(value.unwrap(), "%Y-%m-%d %H:%M:%S")
+                        .unwrap()
+                        .and_local_timezone(local_tz)
+                        .unwrap()
+                        .to_rfc3339();
+                }
                 _ => {}
             }
         }
