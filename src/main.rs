cc1e43113074e7585b5ff70156421394380a8092
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-11 09:08:30 +0300

    feat: use statement.next() instead of connection.iterate()

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,37 @@
 use chrono::{Local, NaiveDateTime};
 use comfy_table::Table;
+use sqlite::State;
+
 fn main() {
     let connection = sqlite::open("../hamster_test.db").unwrap();
-    let query = "
+    let mut statement = connection
+        .prepare(
+            "
         SELECT start_time, name, description
         FROM facts
         LEFT JOIN activities
         ON activities.id=facts.activity_id
         ORDER BY facts.id DESC
         LIMIT 5;
-    ";
+    ",
+        )
+        .unwrap();
 
     let mut table = Table::new();
     let local_tz = Local::now().timezone();
 
     table.set_header(vec!["start_time", "name"]);
 
-    let process_row = |pairs: &[(&str, Option<&str>)]| -> bool {
-        let mut fact_dt: String = String::new();
-        let mut fact_name = String::new();
-        for &(name, value) in pairs.iter() {
-            match name {
-                "name" => fact_name = String::from(value.unwrap()),
-                "start_time" => {
-                    fact_dt = NaiveDateTime::parse_from_str(value.unwrap(), "%Y-%m-%d %H:%M:%S")
-                        .unwrap()
-                        .and_local_timezone(local_tz)
-                        .unwrap()
-                        .to_rfc3339();
-                }
-                _ => {}
-            }
-        }
+    while let Ok(State::Row) = statement.next() {
+        let fact_name = statement.read::<String, _>("name").unwrap();
+        let start_time_raw = statement.read::<String, _>("start_time").unwrap();
+        let fact_dt = NaiveDateTime::parse_from_str(&start_time_raw, "%Y-%m-%d %H:%M:%S")
+            .unwrap()
+            .and_local_timezone(local_tz)
+            .unwrap()
+            .to_rfc3339();
         table.add_row(vec![fact_dt, fact_name]);
+    }
 
-        true
-    };
-
-    connection.iterate(query, process_row).unwrap();
     println!("{table}");
 }
