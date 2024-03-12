f10391a6103518f05542d0a01be5241440613f10
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-12 09:28:26 +0300

    feat: implement week_start()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use chrono::{Local, NaiveDateTime};
+use chrono::{DateTime, Datelike, Days, Local, NaiveDateTime};
 use comfy_table::Table;
 use sqlite::State;
+
+fn week_start(dt: DateTime<Local>) -> DateTime<Local> {
+    dt.checked_sub_days(Days::new(dt.weekday().num_days_from_monday() as u64))
+        .unwrap()
+        .date_naive()
+        .and_hms_opt(0, 0, 0)
+        .unwrap()
+        .and_local_timezone(dt.timezone())
+        .unwrap()
+}
 
 fn main() {
     let connection = sqlite::open("../hamster_test.db").unwrap();
