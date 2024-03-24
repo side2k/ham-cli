92c21383cd9bc9b4eeb49860ed6b0f7eea7d2c43
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-24 22:19:16 +0300

    feat: add cli module with Info and GetFacts commands

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Outputs information
+    Info {},
+    /// get facts (last week by default)
+    GetFacts {},
+}
