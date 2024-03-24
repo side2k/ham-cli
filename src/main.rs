450b8a5a3495aec56405a3daf8e8b77ab254bae8
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-24 22:19:53 +0300

    feat: output facts only on get-facts command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use chrono::Local;
+use clap::Parser;
 use comfy_table::Table;
+mod cli;
 mod hamster;
 mod utils;
 
 fn main() {
+    let cli_args = cli::Cli::parse();
+    match cli_args.command {
+        cli::Commands::GetFacts {} => print_last_week_facts(),
+        _ => {
+            println!("This command is not implemented yet")
+        }
+    }
+}
+
+fn print_last_week_facts() {
     let hamster_data = hamster::HamsterData::open().unwrap();
     let facts = hamster_data.get_facts(utils::week_start(Local::now()));
     let mut table = Table::new();
