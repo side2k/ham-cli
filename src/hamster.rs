a9aeccecd032897dd778765756bd31358b434a66
Leonid Amirov <leonid.amirov@gmail.com>
2024-03-26 08:46:08 +0300

    feat(hamster): query end_time too

--- src/hamster.rs
+++ src/hamster.rs
@@ -4,6 +4,7 @@ use sqlite::State;
 pub struct FactRecord {
     pub name: String,
     pub start_time: DateTime<Local>,
+    pub end_time: Option<DateTime<Local>>,
 }
 
 pub struct HamsterData {
@@ -23,7 +24,7 @@ impl HamsterData {
             .connection
             .prepare(
                 "
-                SELECT start_time, name, description
+                SELECT start_time, end_time, name, description
                 FROM facts
                 LEFT JOIN activities
                 ON activities.id=facts.activity_id
@@ -43,6 +44,7 @@ impl HamsterData {
         while let Ok(State::Row) = statement.next() {
             data.push(FactRecord {
                 name: statement.read::<String, _>("name").unwrap(),
+
                 start_time: NaiveDateTime::parse_from_str(
                     statement.read::<String, _>("start_time").unwrap().as_str(),
                     "%Y-%m-%d %H:%M:%S",
@@ -50,6 +52,23 @@ impl HamsterData {
                 .unwrap()
                 .and_local_timezone(local_tz)
                 .unwrap(),
+
+                end_time: {
+                    let end_time_raw = statement.read::<Option<String>, _>("end_time").unwrap();
+
+                    match end_time_raw {
+                        Some(end_time_str) => Some(
+                            NaiveDateTime::parse_from_str(
+                                end_time_str.as_str(),
+                                "%Y-%m-%d %H:%M:%S",
+                            )
+                            .unwrap()
+                            .and_local_timezone(local_tz)
+                            .unwrap(),
+                        ),
+                        None => None,
+                    }
+                },
             });
         }
         data
