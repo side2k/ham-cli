c2c13091a7bf016043d212cd293fb03b725d0fd4
Leonid Amirov <leonid.amirov@gmail.com>
2024-04-01 09:06:54 +0300

    feat(utils): implement duration_str()

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Datelike, Days, Local};
+use std::time::Duration;
 
 pub fn week_start(dt: DateTime<Local>) -> DateTime<Local> {
     dt.checked_sub_days(Days::new(dt.weekday().num_days_from_monday() as u64))
@@ -8,4 +9,12 @@ pub fn week_start(dt: DateTime<Local>) -> DateTime<Local> {
         .unwrap()
         .and_local_timezone(dt.timezone())
         .unwrap()
+}
+
+pub fn duration_str(duration: Duration) -> String {
+    let minutes_total = duration.as_secs() / 60;
+    let hours = minutes_total / 60;
+    let minutes = minutes_total % 60;
+
+    String::from(format!("{hh}:{mm:02}", hh = hours, mm = minutes))
 }
