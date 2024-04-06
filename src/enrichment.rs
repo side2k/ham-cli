5326b517991a33dea1e70818ab0940ca1b849ae2
Leonid Amirov <leonid.amirov@gmail.com>
2024-04-07 01:21:34 +0300

    feat(enrichment): implement HamsterEnrichedData.task() trait method for HamsterFact

---
+++ src/enrichment.rs
@@ -0,0 +1,30 @@
+use crate::hamster::HamsterFact;
+use crate::utils::{LinkText, MarkdownProcessing};
+use markdown::ParseOptions;
+
+pub struct TaskLink {
+    pub link_title: String,
+    pub href: String,
+}
+
+pub trait HamsterEnrichedData {
+    fn task(&self) -> Option<TaskLink>;
+}
+
+impl HamsterEnrichedData for HamsterFact {
+    fn task(&self) -> Option<TaskLink> {
+        let markdown_root =
+            markdown::to_mdast(&self.description, &ParseOptions::default()).unwrap();
+        let links = markdown_root.links();
+
+        if links.len() > 0 {
+            let link = links[0];
+            Some(TaskLink {
+                link_title: link.text(),
+                href: link.url.clone(),
+            })
+        } else {
+            None
+        }
+    }
+}
